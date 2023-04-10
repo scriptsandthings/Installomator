933ab33781b4e003b0de2740f08e4305385cf350
Scripts and Things <ShitttyScripts@gmail.com>
2023-04-10 16:57:22 -0400

    create label - mailtooutlook
    label for MailToOutlook_2.0.pkg from macadmins.software
    
    Only one version so it's a direct download link.
    
    ## Output >>>>
    Installomator % sudo ./assemble.sh mailtooutlook DEBUG=0
    2023-04-10 16:54:14 : INFO  : mailtooutlook : setting variable from argument DEBUG=0
    2023-04-10 16:54:14 : REQ   : mailtooutlook : ################## Start Installomator v. 10.4beta, date 2023-04-10
    2023-04-10 16:54:14 : INFO  : mailtooutlook : ################## Version: 10.4beta
    2023-04-10 16:54:14 : INFO  : mailtooutlook : ################## Date: 2023-04-10
    2023-04-10 16:54:14 : INFO  : mailtooutlook : ################## mailtooutlook
    2023-04-10 16:54:14 : INFO  : mailtooutlook : BLOCKING_PROCESS_ACTION=tell_user
    2023-04-10 16:54:14 : INFO  : mailtooutlook : NOTIFY=success
    2023-04-10 16:54:14 : INFO  : mailtooutlook : LOGGING=INFO
    2023-04-10 16:54:14 : INFO  : mailtooutlook : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2023-04-10 16:54:14 : INFO  : mailtooutlook : Label type: pkg
    2023-04-10 16:54:14 : INFO  : mailtooutlook : archiveName: MailToOutlook_2.0.pkg
    2023-04-10 16:54:14 : INFO  : mailtooutlook : no blocking processes defined, using MailToOutlook_2.0 as default
    2023-04-10 16:54:14 : INFO  : mailtooutlook : No version found using packageID com.microsoft.MailToOutlook.component com.microsoft.MailToOutlookV2
    2023-04-10 16:54:14 : INFO  : mailtooutlook : name: MailToOutlook_2.0, appName: MailToOutlook_2.0.app
    2023-04-10 16:54:14.928 mdfind[75285:457789] [UserQueryParser] Loading keywords and predicates for locale "en_US"
    2023-04-10 16:54:14.928 mdfind[75285:457789] [UserQueryParser] Loading keywords and predicates for locale "en"
    2023-04-10 16:54:14.980 mdfind[75285:457789] Couldn't determine the mapping between prefab keywords and predicates.
    2023-04-10 16:54:14 : WARN  : mailtooutlook : No previous app found
    2023-04-10 16:54:15 : WARN  : mailtooutlook : could not find MailToOutlook_2.0.app
    2023-04-10 16:54:15 : INFO  : mailtooutlook : appversion:
    2023-04-10 16:54:15 : INFO  : mailtooutlook : Latest version not specified.
    2023-04-10 16:54:15 : REQ   : mailtooutlook : Downloading https://macadmins.software/tools/MailToOutlook_2.0.pkg to MailToOutlook_2.0.pkg
    2023-04-10 16:54:15 : REQ   : mailtooutlook : no more blocking processes, continue with update
    2023-04-10 16:54:15 : REQ   : mailtooutlook : Installing MailToOutlook_2.0
    2023-04-10 16:54:15 : INFO  : mailtooutlook : Verifying: MailToOutlook_2.0.pkg
    2023-04-10 16:54:15 : INFO  : mailtooutlook : Team ID: QGS93ZLCU7 (expected: QGS93ZLCU7 )
    2023-04-10 16:54:15 : INFO  : mailtooutlook : Installing MailToOutlook_2.0.pkg to /
    2023-04-10 16:54:18 : INFO  : mailtooutlook : Finishing...
    2023-04-10 16:54:21 : INFO  : mailtooutlook : No version found using packageID com.microsoft.MailToOutlook.component com.microsoft.MailToOutlookV2
    2023-04-10 16:54:21 : INFO  : mailtooutlook : name: MailToOutlook_2.0, appName: MailToOutlook_2.0.app
    2023-04-10 16:54:21.543 mdfind[75447:458251] [UserQueryParser] Loading keywords and predicates for locale "en_US"
    2023-04-10 16:54:21.543 mdfind[75447:458251] [UserQueryParser] Loading keywords and predicates for locale "en"
    2023-04-10 16:54:21.598 mdfind[75447:458251] Couldn't determine the mapping between prefab keywords and predicates.
    2023-04-10 16:54:21 : WARN  : mailtooutlook : No previous app found
    2023-04-10 16:54:21 : WARN  : mailtooutlook : could not find MailToOutlook_2.0.app
    2023-04-10 16:54:21 : REQ   : mailtooutlook : Installed MailToOutlook_2.0
    2023-04-10 16:54:21 : INFO  : mailtooutlook : notifying
    2023-04-10 16:54:22 : INFO  : mailtooutlook : App not closed, so no reopen.
    2023-04-10 16:54:22 : REQ   : mailtooutlook : All done!

---
+++ fragments/labels/mailtooutlook.sh
@@ -0,0 +1,8 @@
+mailtooutlook)
+    name="MailToOutlook_2.0"
+    type="pkg"
+    packageID="com.microsoft.MailToOutlook.component com.microsoft.MailToOutlookV2"
+    downloadURL="https://macadmins.software/tools/MailToOutlook_2.0.pkg"
+    appNewVersion=""
+    expectedTeamID="QGS93ZLCU7"
+    ;;
